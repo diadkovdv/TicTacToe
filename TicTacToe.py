873167f3b6a73f480591e2c5f7cdc200bfa4b625
diadkovdv <87365761+diadkovdv@users.noreply.github.com>
2021-07-29 04:27:52 +0300

    Add files via upload

---
+++ TicTacToe.py
@@ -0,0 +1,14 @@
+
+def draw_board():
+    game_board = [
+    ['  ', ' 0 ', ' 1 ',' 2 '],
+    [' 0', ' - ', ' - ',' - '],
+    [' 1', ' - ', ' - ',' - '],
+    [' 2', ' - ', ' - ',' - ']]
+
+
+    print(''.join(game_board[0][:]))
+    print(''.join(game_board[1][:]))
+    print(''.join(game_board[2][:]))
+    print(''.join(game_board[3][:]))
+
